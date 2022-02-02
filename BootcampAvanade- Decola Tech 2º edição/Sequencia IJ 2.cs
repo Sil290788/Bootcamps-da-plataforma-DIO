6d94f206e5b70eeac23a930510d96b16d7efee1e
Silvia Morais <silvia_tholina@hotmail.com>
2022-02-02 19:13:36 -0300

    Create Sequencia IJ 2.cs
    Princípios básicos/ básico

---
+++ BootcampAvanade- Decola Tech 2º edição/Sequencia IJ 2.cs
@@ -0,0 +1,28 @@
+﻿/* Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+Entrada
+Não há nenhuma entrada neste problema.
+
+Saída
+Imprima a sequencia conforme exemplo abaixo */
+
+
+using System;
+
+class DIO
+{
+
+    static void Main(string[] args)
+    {
+        int i = 1;
+
+        while (i <= 9)
+        {
+            for (int j = 7; j > 4; j--)
+            {
+                Console.WriteLine($"I={i} J={j} ");
+            }
+            i += 2;
+        }
+    }
+}
