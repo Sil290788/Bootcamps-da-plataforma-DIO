9826294b5f4b67e025785b2c600f172deb89a940
Silvia Morais <silvia_tholina@hotmail.com>
2022-02-02 19:20:09 -0300

    Básico/ básico

---
+++ BootcampAvanade- Decola Tech 2º edição/Sucessor par.cs
@@ -0,0 +1,27 @@
+﻿/* Para se preparar para os outros problemas, vamos fazer um teste. Dado um número X, retorne o menor número par maior do que X. 
+ 
+ ENTRADA: Uma linha contendo um número 0 < X 
+< 107
+
+SAÍDA: Uma linha contendo a resposta do problema
+*/
+
+using System;
+
+class DIO
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y;
+        if (x % 2 == 0)
+        {
+            y = x + 2;
+        }
+        else
+        {
+            y = x + 1;
+        }
+        Console.WriteLine(y);
+    }
+}
